145ad177f24d23cf8263ee98ec254a74140eec80
remolueoend <remolueoend@users.noreply.github.com>
2020-10-29 08:52:50 +0100

    add examples week 7

---
+++ general_examples/week7/LinkedList.java
@@ -0,0 +1,8 @@
+public class LinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ general_examples/week7/ValueVsReference.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+class Vector {
+	int x;
+	int y;
+	int z;
+	
+	Vector(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
+
+public class ValueVsReference {
+
+	public static void main(String[] args) {
+		int integer = 5;
+		int[] array = {1, 2, 3};
+		String string = "hello";
+		Vector vector = new Vector(1, 2, 3);
+		
+		updateInteger(integer);
+		updateArray(array);
+		updateString(string);
+		updateVector(vector);
+		
+		System.out.println("integer: " + integer);
+		System.out.println("array: " + Arrays.toString(array));
+		System.out.println("string: " + string);
+		System.out.println("vector: " + vector.x + ", " + vector.y + ", " + vector.z);
+	}
+	
+	
+	
+	
+	
+	
+	public static void updateInteger(int i) {
+		i = i+1;
+	}
+	
+	public static void updateArray(int[] arr) {
+		arr[1] = 100;
+	}
+	
+	public static void updateString(String str) {
+		str = "world";
+	}
+	
+	public static void updateVector(Vector v) {
+		v.x = 100;
+	}
+}
+
