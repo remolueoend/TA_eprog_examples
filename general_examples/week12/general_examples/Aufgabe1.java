ae0fdd93fa1c1c7b38586072f7ea240a80fc4ee6
remolueoend <remolueoend@users.noreply.github.com>
2020-12-02 20:04:33 +0100

    add exercises for week 12

---
+++ general_examples/week12/general_examples/Aufgabe1.java
@@ -0,0 +1,30 @@
+package general_examples;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/*
+schreiben Sie ein Programm das eine Datei Word fuer Word einliest und die Woerter der Datei
+als Liste speichert.  
+
+zuerst sollen Sie alle eingelesenen Woerter ausgeben.
+
+dann sollen Sie alle Woerter in der umgekehrten Reihenfolge ausgeben.
+
+dann geben Sie alle Substantive (fangen mit Grossbuchstaben an) in Grossbuchstaben aus.
+
+     Also Haus ->  HAUS
+
+Dann geben Sie die Woerter ohne Substantive aus
+*/
+
+public class Aufgabe1 {
+	public static void main(String[] args) throws FileNotFoundException {
+		File inputFile = new File("week12/aufgabe1_in.txt");
+		Scanner in = new Scanner(inputFile);
+		
+		// TODO: Methode implementieren
+	}
+}
---
+++ general_examples/week12/general_examples/Aufgabe2.java
@@ -0,0 +1,29 @@
+package general_examples;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/*
+Schreiben Sie ein Programm das eine Datei mit ganzen Zahlen liest und dann die Zahlen als
+Liste druckt:
+
+Dann drucken Sie den Durchschnitt der Zahlen.
+
+Geben Sie die groesste und die kleinste Zahl aus.
+
+Filtern (d.h. entfernen) Sie alle geraden Zahlen (Zahlen die durch 2 ohne Rest teilbar
+sind).
+*/
+
+public class Aufgabe2 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File inputFile = new File("week12/aufgabe2_in.txt");
+		Scanner in = new Scanner(inputFile);
+
+		// TODO: Methode implementieren
+	}
+
+}
