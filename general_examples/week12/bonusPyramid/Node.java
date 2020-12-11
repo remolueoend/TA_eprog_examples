7d802ce53f7deeeb87db0e037052fcafb09e3027
sam <sam@ubuntu-sam>
2020-12-11 16:29:39 +0100

    Bonus u10 von Jean Marc

---
+++ general_examples/week12/bonusPyramid/Node.java
@@ -0,0 +1,29 @@
+package bonusPyramid;
+//Veraendern Sie diese Datei nicht!
+
+public class Node {
+	
+	private Node left, right;
+	
+	public Node(Node left, Node right) {
+		this.setLeft(left);
+		this.setRight(right);
+	}
+
+	public Node getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+
+	public Node getRight() {
+		return right;
+	}
+
+	public void setRight(Node right) {
+		this.right = right;
+	}
+
+}
