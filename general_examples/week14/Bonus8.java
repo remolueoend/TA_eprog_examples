de61e3cd7d5e4e700f53a4615a73f5d422de0a14
remolueoend <remolueoend@users.noreply.github.com>
2020-12-16 21:21:01 +0100

    add examples for week 14

---
+++ general_examples/week14/Bonus8.java
@@ -0,0 +1,69 @@
+import java.util.Arrays;
+
+/*
+Eingabe: 'hans'
+Annahme: Wir haben Zugriff auf alle Teilwörter von 'ans' (ohne h):
+''     // der leere String ist auch ein Teilwort!
+s
+n
+ns
+a
+as
+an
+ans
+
+Wir kopieren nun diese Liste und fügen jedem Eintrag den Präfix 'h' hinzu:
+h
+hs
+hn
+hns
+ha
+has
+han
+hans
+
+Ihr könnt euch selber vergewissern, dass diese zwei Listen kombiniert alle Teilwörter von 'hans' enthalten => Lösung gefunden!
+Wir müssen jetzt nur noch rausfinden, wir wir diese zuvor als gegebene Liste von Teilwörtern von 'ans' erhalten.
+=> Das Problem ist das Gleiche wie das Ursprungsproblem, nun aber für die Eingabe 'ans' statt 'hans'! => Rekursion anwenden:
+
+FindeTeilwörter(eingabe)
+	if länge(eingabe) == 0:
+		return [""]                        // *Base-Case*: das einzige Teilwort eines leeren Strings ist der leere String selbst
+
+	p <= der erste Buchstabe von 'eingabe' // das ist eine Zuweisung zur Variable p
+	tail <= 'eingabe' ohne 'p'
+	
+	teilwörter <= FindeTeilWörter(tail)    // suche *rekursiv* alle Teilwörter von 'tail', also von der Eingabe ohne den ersten Buchstaben
+	                                       // 'teilwörter' für 'ans' wäre also die im Beispiel oben angegebene Liste
+	                                       
+	for wort in teilwörter:                // Ergänze die Liste mit einer Kopie, wobei wir jedem Teilwort unser Präfix/erster Buchstabe vorne anhängen
+		teilwörter.add(p + wort)
+		
+	return teilwörter
+ */
+
+public class Bonus8 {
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(findeTeilworter("hans")));
+	}
+
+	public static String[] findeTeilworter(String eingabe) {
+		if(eingabe.length() == 0) {
+			return new String[] { "" };
+		}
+		
+		char p = eingabe.charAt(0); // erster Buchstabe der Eingabe
+		String[] kurze_teilwoerter = findeTeilworter(eingabe.substring(1)); // *rekursiver* Aufruf mit Eingabe ohne erstem Buchstaben
+		
+		String[] alle_teilwoerter = new String[kurze_teilwoerter.length * 2]; // Die finale Liste beinhaltet doppelt so viele Teilwörter
+		// wir kopieren zuerst einmal die kürzeren Teilwörter (1. Liste) in das finale Array:
+		System.arraycopy(kurze_teilwoerter, 0, alle_teilwoerter, 0, kurze_teilwoerter.length);
+		// Jetzt fügen wir jedes Wort nochmals hinzu, mit dem Präfix 'p':
+		for(int i = 0; i < kurze_teilwoerter.length; i++) {
+			alle_teilwoerter[kurze_teilwoerter.length + i] = p + kurze_teilwoerter[i];
+		}
+		
+		return alle_teilwoerter;
+	}
+}
---
+++ general_examples/week14/bonus_12/MyListImpl.java
@@ -0,0 +1,183 @@
+package bonus_12;
+
+import java.util.Iterator;
+
+class MyNode<T> implements MyListNode<T> {
+
+	private T nodeValue;
+	private MyListNode<T> nextNode;
+
+	public MyNode(T value) {
+		this.nodeValue = value;
+	}
+
+	@Override
+	public T value() {
+		return nodeValue;
+	}
+
+	@Override
+	public void setValue(T value) {
+		this.nodeValue = value;
+
+	}
+
+	@Override
+	public boolean hasNext() {
+		return this.nextNode != null;
+	}
+
+	@Override
+	public MyListNode<T> next() {
+		return this.nextNode;
+	}
+
+	@Override
+	public void setNext(MyListNode<T> node) {
+		this.nextNode = node;
+	}
+
+}
+
+class MyListIterator<T> implements Iterator<T> {
+	MyListNode<T> next;
+
+	public MyListIterator(MyListNode<T> first) {
+		this.next = first;
+	}
+
+	@Override
+	public boolean hasNext() {
+		return this.next != null;
+	}
+
+	@Override
+	public T next() {
+		T value = this.next.value();
+		this.next = this.next.next();
+		return value;
+	}
+}
+
+public class MyListImpl<T> implements MyList<T> {
+	private MyListNode<T> first;
+	private MyListNode<T> last;
+	private int size = 0;
+
+	@Override
+	public T get(int index) {
+		return this.getNode(index).value();
+	}
+
+	@Override
+	public MyListNode<T> getNode(int index) {
+		MyListNode<T> node = this.first;
+		for (int i = 0; i < index; i++) {
+			node = node.next();
+		}
+		return node;
+	}
+
+	@Override
+	public void set(int index, T value) {
+		this.getNode(index).setValue(value);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.size == 0;
+	}
+
+	@Override
+	public int getSize() {
+		return this.size;
+	}
+
+	@Override
+	public void addFirst(T value) {
+		MyNode<T> node = new MyNode<T>(value);
+		if (this.isEmpty()) {
+			this.first = this.last = node;
+		} else {
+			node.setNext(this.first);
+			this.first = node;
+		}
+		this.size++;
+	}
+
+	@Override
+	public void addLast(T value) {
+		MyNode<T> node = new MyNode<T>(value);
+		if (this.isEmpty()) {
+			this.first = this.last = node;
+		} else {
+			this.last.setNext(node);
+			this.last = node;
+		}
+		this.size++;
+	}
+
+	@Override
+	public void addAll(MyList<T> other) {
+		if(other.isEmpty()) return;
+		
+		MyListNode<T> otherFirst = other.getNode(0);
+		MyListNode<T> otherLast = other.getNode(other.getSize() - 1); // Laufzeit hängt von Implementation von other ab!
+
+		if (this.isEmpty()) {
+			this.first = other.getNode(0);
+			this.last = otherLast;
+		} else {
+			this.last.setNext(otherFirst);
+			this.last = otherLast; 
+		}
+		this.size += other.getSize();
+	}
+
+	@Override
+	public T removeFirst() {
+		if (this.isEmpty()) {
+			throw new IllegalAccessError("list is empty");
+		}
+		T value = this.first.value();
+		this.first = this.first.next();
+		this.size--;
+		if (this.isEmpty()) {
+			this.last = null;
+		}
+		return value;
+	}
+
+	@Override
+	public T removeLast() {
+		if (this.isEmpty()) {
+			throw new IllegalAccessError("list is empty");
+		}
+		MyListNode<T> prev = null;
+		MyListNode<T> curr = this.first;
+		while (curr != this.last) {
+			prev = curr;
+			curr = curr.next();
+		}
+		if (prev == null) {
+			this.first = this.last = null;
+		} else {
+			prev.setNext(null);
+			this.last = prev;
+		}
+		this.size--;
+		return curr.value();
+	}
+
+	@Override
+	public void clear() {
+		this.first = this.last = null;
+		this.size = 0;
+
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		return new MyListIterator<T>(this.first);
+	}
+}
